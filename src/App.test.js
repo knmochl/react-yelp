57864bb5499526bd3a446a00b4d6a4137373ca6a
Nathan Sullivan <nsullivan@netsuite.com>
2017-06-21 19:31:35 -0700

    Add a test for router existence

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { shallow } from 'enzyme';
 import App from './App';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
+});
+
+it('has a Router component', () => {
+  const wrapper = shallow(<App />);
+  expect(wrapper.find('Router')).toHaveLength(1);
 });
