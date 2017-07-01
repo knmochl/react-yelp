0543ec3702792197e576087fe07d1167c226c11d
Nathan Sullivan <nsullivan@netsuite.com>
2017-07-01 19:47:51 +0200

    Add Header component to main view component

--- src/views/Main/Container.js
+++ src/views/Main/Container.js
@@ -1,9 +1,12 @@
 import React from 'react'
 
+import Header from '../../components/Header/Header'
+
 export class Container extends React.Component {
   render() {
     return (
       <div>
+        <Header />
         Hello from the container
       </div>
     )
