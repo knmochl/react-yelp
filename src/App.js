e4daef4f523a75d5b64b4fe626f09ba927e23676
Nathan Sullivan <nsullivan@netsuite.com>
2017-07-01 19:24:57 +0200

    Routes files are not needed for router v4

--- src/App.js
+++ src/App.js
@@ -1,22 +1,20 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import './App.css';
-import topRoutes from './routes'
+import Container from './views/Main/Container';
 
 import 'font-awesome/css/font-awesome.css';
 
 
 class App extends Component {
 
-  get content() {
-    return topRoutes()
-  }
-
   render() {
     return (
-      <div style={ { height: '100%' } }>
-        {this.content}
-      </div>
+      <Router>
+        <div>
+          <Route path="/" component={Container} />
+        </div>
+      </Router>
     )
   }
 }
