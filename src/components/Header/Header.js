5e147cc3adb4e3d388d948551b71593a165a55bd
Nathan Sullivan <nsullivan@netsuite.com>
2017-07-02 19:35:05 -0700

    Add some global styles and styles for the header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -3,7 +3,31 @@ import { Link } from 'react-router-dom'
 import styled from 'styled-components'
 
 const Topbar = styled.div`
-  border: 1px solid red;
+  position: fixed;
+  z-index: 10;
+  top: 0;
+  left: 0;
+  background: #48b5e9;
+  width: 100%;
+  padding: 0 25px;
+  height: 80px;
+  line-height: 80px;
+  color: #fff;
+
+  a {
+    text-transform: uppercase;
+    text-decoration: none;
+    letter-spacing: 1px;
+
+    line-height: 40px;
+    h1 { font-size: 28px; }
+  }
+
+  section {
+    position: absolute;
+    top: 0px;
+    right: 25px;
+  }
 `
 
 export class Header extends React.Component {
