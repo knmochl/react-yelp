026547a3b59bae7bcf0de4db674047eb3d75e10b
Nathan Sullivan <nsullivan@netsuite.com>
2017-07-02 18:53:17 -0700

    Add a call to google places api

--- src/views/Main/Container.js
+++ src/views/Main/Container.js
@@ -1,15 +1,32 @@
 import React from 'react'
 import Map, { GoogleApiWrapper } from 'google-maps-react'
 
+import { searchNearby } from '../../utils/googleApiHelpers'
 import Header from '../../components/Header/Header'
 
 export class Container extends React.Component {
+  onReady(mapProps, map) {
+    const {google} = this.props
+    const opts = {
+      location: map.center,
+      radius: '500',
+      types: ['cafe']
+    }
+    searchNearby(google, map, opts)
+      .then((results, pagination) => {
+        // We got some results and a pagination object
+      }).catch((status, result) => {
+        // There was an error
+      })
+  }
+
   render() {
     return (
       <div>
         <Header />
         Hello from the container
         <Map
+          onReady={this.onReady.bind(this)}
           google={this.props.google} />
       </div>
     )
