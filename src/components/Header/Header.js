c7dcaa60e890f8ebad72c33ea1c25f37581ba2cc
Nathan Sullivan <nsullivan@netsuite.com>
2017-07-02 19:22:24 -0700

    Add a dummy style to header, just to see if it works

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,15 +1,20 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import styled from 'styled-components'
+
+const Topbar = styled.div`
+  border: 1px solid red;
+`
 
 export class Header extends React.Component {
   render() {
     return (
-      <div>
+      <Topbar>
         <Link to="/"><h1>Yelp</h1></Link>
         <section>
           Fullstack.io
         </section>
-      </div>
+      </Topbar>
     )
   }
 }
