975d0ed8fe52221bbbfc002c8ba0d8f9c6f6abbe
Nathan Sullivan <nsullivan@netsuite.com>
2017-07-06 21:20:11 -0700

    Add colors to css constants

--- src/lib/css-constants.js
+++ src/lib/css-constants.js
@@ -2,6 +2,13 @@ const cssVars = {
   topbar: {
     height: "80px",
   },
+  colors: {
+    dark: "#404040",
+    lightgray: "#a2a2a2",
+    white: "#ffffff",
+    highlight: "#48b5e9",
+    headingcolor: "#48b5e9",
+  },
 }
 
 export default cssVars
