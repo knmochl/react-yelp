37ef5462525272b1463cb2c3a57d264736b2b174
Nathan Sullivan <nsullivan@netsuite.com>
2017-07-07 22:45:21 -0700

    Add a dummy map display component

---
+++ src/views/Main/Map/Map.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const StyledMap = styled.div`
+`
+
+export class MapComponent extends React.Component {
+  render() {
+    return (
+      <StyledMap>
+        MAP!
+      </StyledMap>
+    )
+  }
+}
+
+export default MapComponent
